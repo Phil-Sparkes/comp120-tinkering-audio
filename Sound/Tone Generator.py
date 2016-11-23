d3ed2eba1c38c5eacf2393be41380937f3eb0877
Phil-Sparkes <phil.sparkes95@gmail.com>
2016-11-23 09:08:45 +0000

    added last weeks tinkering audio worksheet

---
+++ Sound/Tone Generator.py
@@ -0,0 +1,36 @@
+import wave
+import struct
+import math
+
+noise_out = wave.open('noise.wav', 'w')
+
+noise_out.setnchannels(1)
+noise_out.setsampwidth(2)
+noise_out.setframerate(44100)
+noise_out.setnframes(44100)
+noise_out.setcomptype('NONE', 'not compressed')
+
+values = []
+
+print noise_out.getparams()
+
+Sample_Length = noise_out.getframerate()
+Channels = noise_out.getnchannels()
+Frequency = 8000
+Sample_Rate = Sample_Length
+Volume = 0.5
+Bit_Depth = 2**15 - 1
+
+for i in xrange(Sample_Length):
+    value = math.sin(2.0 * math.pi * Frequency * (i / 44100.0)) * (Volume * Bit_Depth)
+    print value
+
+    packaged_value = struct.pack("<h",value)
+
+    for j in xrange(Channels):
+        values.append(packaged_value)
+
+value_str = ''.join(values)
+noise_out.writeframes(value_str)
+
+noise_out.close()
---
+++ Sound/main.py
@@ -0,0 +1,51 @@
+import wave
+import struct
+
+soundWave = wave.open('noise2.wav', 'r')
+# mode must be 'r', 'rb', 'w', or 'wb'
+
+length = soundWave.getnframes()
+frames = []
+
+
+
+for i in xrange(length):
+    waveData = soundWave.readframes(1)
+    data = struct.unpack("<h", waveData)
+    frames.append(int(data[0]))
+
+print soundWave.getparams()
+
+def increase_volume(frames, length):
+    """Doubles the volume"""
+    for i in xrange(length):
+        frames[i] *= 2
+
+def max():
+    """finds the highest value"""
+    maxnum = 0
+    for i in xrange(length):
+        challenger = frames[i]
+        if abs(challenger) > maxnum:
+            maxnum = abs(challenger)
+    return maxnum
+
+def count_sign_changes():
+    """checks how many 0s"""
+    numzero = 0
+    for i in xrange(length):
+        if frames[i] == 0:
+            numzero += 1
+    numzero = numzero/3    # 3 seconds
+    numzero = numzero/2
+    return numzero
+
+BitDepth = 2**15 - 1
+Volume = float(max()) / float(BitDepth)
+print frames
+
+print Volume
+print count_sign_changes()
+#increase_volume(frames, length)
+
+
