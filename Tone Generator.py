ef1b2fc416b0c61abe7db0ce73a23f3043a94972
Phil-Sparkes <phil.sparkes95@gmail.com>
2016-11-29 14:31:50 +0000

    Made pep8 happy again

--- Tone Generator.py
+++ Tone Generator.py
@@ -13,7 +13,8 @@ noise_out.setnframes(44100)
 noise_out.setcomptype('NONE', 'not compressed')
 Channels = noise_out.getnchannels()
 
-def sineWave(frequency, volume, attack_time, sustain_time, release_time):
+
+def sine_wave(frequency, volume, attack_time, sustain_time, release_time):
     """Envelope function, plays a pure tone but fades into and out of it"""
     # Creates volume_change as a float
     volume_change = 0.00
@@ -22,14 +23,14 @@ def sineWave(frequency, volume, attack_time, sustain_time, release_time):
     values = []
 
     # Sets the sampling rate
-    samplingRate = 44100
+    sampling_rate = 44100
 
     # creates an interval between samples
     interval = 1.0 / frequency
-    samplesPerCycle = interval * samplingRate
+    samples_per_cycle = interval * sampling_rate
 
     # not sure what that one does
-    maxCycle = 2 * math.pi
+    max_cycle = 2 * math.pi
 
     # loops for length of the pure tone
     for pos in range((attack_time + sustain_time + release_time)):
@@ -41,47 +42,49 @@ def sineWave(frequency, volume, attack_time, sustain_time, release_time):
             volume_change -= (float(volume) / float(release_time))
 
         # Creates the sample
-        rawSample = math.sin((pos / samplesPerCycle) * maxCycle)
-        sampleVal = int(volume_change * rawSample)
+        raw_sample = math.sin((pos / samples_per_cycle) * max_cycle)
+        sample_value = int(volume_change * raw_sample)
 
         # Packages the and appends the sample
-        packaged_value = struct.pack("<h", sampleVal)
+        packaged_value = struct.pack("<h", sample_value)
         for j in xrange(Channels):
             values.append(packaged_value)
     value_str = ''.join(values)
     noise_out.writeframes(value_str)
 
 # Dictionary of notes
-notes = {'a': 440.0 * 2.0 ** (0 / 12.0),
-        'a#': 440.0 * 2.0 ** (1 / 12.0),
-        'b': 440.0 * 2.0 ** (2 / 12.0),
-        'c': 440.0 * 2.0 ** (3 / 12.0),
-        'c#': 440.0 * 2.0 ** (4 / 12.0),
-        'd': 440.0 * 2.0 ** (5 / 12.0),
-        'd#': 440.0 * 2.0 ** (6 / 12.0),
-        'e': 440.0 * 2.0 ** (7 / 12.0),
-        'f': 440.0 * 2.0 ** (8 / 12.0),
-        'f#': 440.0 * 2.0 ** (9 / 12.0),
-        'g': 440.0 * 2.0 ** (10 / 12.0),
-        'g#': 440.0 * 2.0 ** (11 / 12.0),
-         'a2': 440.0 * 2.0 ** (12 / 12.0)}
+notes = {
+    'a': 440.0 * 2.0 ** (0 / 12.0),
+    'a#': 440.0 * 2.0 ** (1 / 12.0),
+    'b': 440.0 * 2.0 ** (2 / 12.0),
+    'c': 440.0 * 2.0 ** (3 / 12.0),
+    'c#': 440.0 * 2.0 ** (4 / 12.0),
+    'd': 440.0 * 2.0 ** (5 / 12.0),
+    'd#': 440.0 * 2.0 ** (6 / 12.0),
+    'e': 440.0 * 2.0 ** (7 / 12.0),
+    'f': 440.0 * 2.0 ** (8 / 12.0),
+    'f#': 440.0 * 2.0 ** (9 / 12.0),
+    'g': 440.0 * 2.0 ** (10 / 12.0),
+    'g#': 440.0 * 2.0 ** (11 / 12.0),
+    'a2': 440.0 * 2.0 ** (12 / 12.0)
+}
 
 # Sets the length of the note in seconds
 note_time = 0.4
 # sets the percentage of the notes attack, sustain and decay times, values add up to 1
-attacktime = int((0.1  * note_time) * 44100)
-sustaintime = int((0.6  * note_time) * 44100)
-decaytime = int((0.3  * note_time) * 44100)
+attacktime = int((0.1 * note_time) * 44100)
+sustaintime = int((0.6 * note_time) * 44100)
+decaytime = int((0.3 * note_time) * 44100)
 
 # sets the volume
 volumenote = 8000
 
 # Twinkle Twikle in list form
-playnotes = ['c','c','g','g','a2','a2','g','f','f','e','e','d','d','c','g','g','f','f','e','e','d','g','g','f','f','e','e','d']
+playnotes = ['c', 'c', 'g', 'g', 'a2', 'a2', 'g', 'f', 'f', 'e', 'e', 'd', 'd', 'c', 'g', 'g', 'f', 'f', 'e', 'e', 'd', 'g', 'g', 'f', 'f', 'e', 'e', 'd']
 
 # creates the notes
 for note in range(len(playnotes)):
-    sineWave(notes[playnotes[note]], volumenote, attacktime, sustaintime, decaytime)
+    sine_wave(notes[playnotes[note]], volumenote, attacktime, sustaintime, decaytime)
 
     # this part is for random notes and random times played for can uncomment if want to try
     """
@@ -89,6 +92,5 @@ for note in range(len(playnotes)):
     attacktime = int((0.1 * note_time) * 44100)
     sustaintime = int((0.6 * note_time) * 44100)
     decaytime = int((0.3 * note_time) * 44100)
-    sineWave(notes[random.choice(['a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e','f', 'f#', 'g', 'g#'])], volumenote, attacktime, sustaintime, decaytime)
+    sine_wave(notes[random.choice(['a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e','f', 'f#', 'g', 'g#'])], volumenote, attacktime, sustaintime, decaytime)
     """
-
